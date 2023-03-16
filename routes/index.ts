9b0908dac77aadc02485bc9a8a679806c5f853e7
alirio <mie21001@byui.edu>
2023-03-16 00:10:10 -0400

    import auth route into index route

--- routes/index.ts
+++ routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import swagger from './swagger';
 import userRouter from './users';
+import authRouter from './auth';
 
 const router = Router();
 
 router.use('/users', userRouter);
+router.use('/auth', authRouter)
 router.use('/', swagger);
 
 export default router;
